3e9b37b5ac115a9281f602427ab1d6730e37e4c1
Gokila-K2593 <gokilakannan2003@gmail.com>
2025-02-16 19:29:29 +0530

    output

--- ques1_task2.py
+++ ques1_task2.py
@@ -1,8 +1,6 @@
 a=int(input("Enter an integer:"))
 reversed_num=0
-remainder=0
 while a !=0:
-    remainder=a%10
-    reversed_num=reversed_num+10+remainder
-    a=a//10
+    reversed_num=int(str(a)[::-1])
     print("The reverse number of", a,"is",reversed_num)
+    break;
