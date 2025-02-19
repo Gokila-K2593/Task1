7b98749dd32d13b45941cb761a95c0dd3c8f2e6a
Gokila-K2593 <gokilakannan2003@gmail.com>
2025-02-19 21:04:14 +0530

    output

--- question1.py
+++ question1.py
@@ -1,4 +1,4 @@
 name = "Gokila"
 age = 21
 height = 160  
-print("My name is "+ name + ",I am "+ str(age) +" years old and my height is " +str(height) +" cm")
+print("My name is ", name,",I am ", str(age), "years old and my height is",str(height),"cm")
