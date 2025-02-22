9a1e82cb7806c6c3576fd267a609e95504934f62
Gokila-K2593 <gokilakannan2003@gmail.com>
2025-02-22 19:19:10 +0530

    output

--- ques4_task2.py
+++ ques4_task2.py
@@ -1,12 +1,11 @@
 a=int(input("Enter the first number:"))
 b=int(input("Enter second number:"))
 print("The range between",a,"and",b)
+count=0
 for i in range(a,b+1):
-    if i>1:
-        prime=True
-        for j in range(2,int(i**0.5)+1):  
+        for j in range(1,i+1):  
             if i%j==0:
-                prime=False 
-                break
-        if prime:
-            print(i)     
+                count=count+1
+        if count==2:
+             print(i)  
+        count=0   
